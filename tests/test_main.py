1e233cb95569cac085526f43cdf9bff527a2f57f
sergiustavila <sergiu.stavila@gmail.com>
2023-11-07 20:04:41 +0200

    Added some API routes

---
+++ tests/test_main.py
@@ -0,0 +1,16 @@
+from fastapi.testclient import TestClient
+from app.main import app
+
+client = TestClient(app)
+
+def test_read_root():
+    response = client.get("/")
+    
+    assert response.status_code == 200
+    assert response.json() == {"message":"Hello World!"}
+
+def test_read_data():
+    response = client.get("/data/sergiu")
+    
+    assert response.status_code == 200
+    assert response.json() == {"message":"Hell on World sergiu"}
