addeccdc344e6a8a5adb4ef3cbfcf2e149474b4a
sergiustavila <sergiu.stavila@gmail.com>
2023-11-09 19:51:28 +0200

    Fixed pylint errors

--- app/main.py
+++ app/main.py
@@ -1,11 +1,16 @@
+""" Module docstring """
 from fastapi import FastAPI
 
 app = FastAPI()
 
+
 @app.get("/")
 async def read_root():
+    """Function docstring"""
     return {"message": "Hello World!"}
 
+
 @app.get("/data/{name}")
+#pylint: disable=C0116
 async def read_data(name: str):
-    return {"message": f"Hell on World {name}"}
+    return {"message": f"Hell on World {name.capitalize()}"}
--- tests/test_main.py
+++ tests/test_main.py
@@ -1,16 +1,21 @@
+""" Unit tests docstring """
 from fastapi.testclient import TestClient
 from app.main import app
 
 client = TestClient(app)
 
+
 def test_read_root():
+    """Function docstring"""
     response = client.get("/")
-    
+
     assert response.status_code == 200
-    assert response.json() == {"message":"Hello World!"}
+    assert response.json() == {"message": "Hello World!"}
+
 
 def test_read_data():
+    """Function docstring"""
     response = client.get("/data/sergiu")
-    
+
     assert response.status_code == 200
-    assert response.json() == {"message":"Hell on World sergiu"}
+    assert response.json() == {"message": "Hell on World sergiu"}
